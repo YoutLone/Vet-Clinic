3f0e3098a82abdc45efe0023f49d8235499f5020
Yout <youtlone95@gmail.com>
2023-08-16 00:04:39 +0630

    add multiple table for vet_clinic database

--- shema.sql
+++ shema.sql
@@ -11,3 +11,36 @@ CREATE TABLE animals (
 
 ALTER TABLE animals 
 ADD COLUMN species VARCHAR(50);
+
+
+-- create owners table
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(200),
+    age INTEGER
+);
+
+-- create species table
+CREATE TABLE species (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (200)
+);
+
+-- update animals table 
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER,
+ADD COLUMN owner_id INTEGER;
+
+-- Add foreign key 
+ALTER TABLE animals
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id)
+REFERENCES species(id);
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_owner
+FOREIGN KEY (owner_id)
+REFERENCES owners(id);
