d407acc7fd614e7262a691f1f8618eeb97730617
Yout <youtlone95@gmail.com>
2023-08-21 17:28:21 +0630

    add all files for performance audit

--- queries.sql
+++ queries.sql
@@ -213,3 +213,8 @@ WHERE vets.name = 'Maisy Smith'
 GROUP BY species.id
 ORDER BY count_visits DESC
 LIMIT 1;
+
+
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
